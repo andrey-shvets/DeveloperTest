8c6ba1f757eefef5401259ad31ef14dd42c1fb8c
andrey-shvets <andrey.shvets.sd@yandex.ru>
2022-02-21 04:24:52 +0200

    Implement DeveloperTestImplementationAsync

--- DeveloperTest/DeveloperTestImplementationAsync.cs
+++ DeveloperTest/DeveloperTestImplementationAsync.cs
@@ -1,28 +1,69 @@
 ﻿#region Copyright statement
 // --------------------------------------------------------------
 // Copyright (C) 1999-2016 Exclaimer Ltd. All Rights Reserved.
-// No part of this source file may be copied and/or distributed 
+// No part of this source file may be copied and/or distributed
 // without the express permission of a director of Exclaimer Ltd
 // ---------------------------------------------------------------
 #endregion
-using System;
+
+using DeveloperTest.Reader;
+using DeveloperTestInterfaces;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
-using DeveloperTestInterfaces;
 
 namespace DeveloperTest
 {
     public sealed class DeveloperTestImplementationAsync : IDeveloperTestAsync
     {
-        public Task RunQuestionOne(ICharacterReader reader, IOutputResult output, CancellationToken cancellationToken)
+        public async Task RunQuestionOne(ICharacterReader reader, IOutputResult output, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var wordReader = new WordReader(reader);
+            var countedWords = new ConcurrentDictionary<string, int>();
+
+            try
+            {
+                while (true)
+                {
+                    var nextWord = await wordReader.GetNextWordAsync();
+                    countedWords.AddOrUpdate(nextWord, 1, (_, v) => v + 1);
+                }
+            }
+            catch (EndOfStreamException)
+            { }
+
+            var sortedWords = countedWords.OrderByDescending(w => w.Value).ThenBy(w => w.Key).Select(w => $"{w.Key} - {w.Value}").ToHashSet();
+
+            foreach (var word in sortedWords)
+                output.AddResult(word);
         }
 
-        public Task RunQuestionTwo(ICharacterReader[] readers, IOutputResult output, CancellationToken cancellationToken)
+        public async Task RunQuestionTwo(ICharacterReader[] readers, IOutputResult output, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var countedWords = new ConcurrentDictionary<string, int>();
+
+            foreach (var reader in readers)
+            {
+                var wordReader = new WordReader(reader);
+
+                try
+                {
+                    while (true)
+                    {
+                        var nextWord = await wordReader.GetNextWordAsync();
+                        countedWords.AddOrUpdate(nextWord, 1, (_, v) => v + 1);
+                    }
+                }
+                catch (EndOfStreamException)
+                { }
+            }
+
+            var sortedWords = countedWords.OrderByDescending(w => w.Value).ThenBy(w => w.Key).Select(w => $"{w.Key} - {w.Value}").ToHashSet();
+
+            foreach (var word in sortedWords)
+                output.AddResult(word);
         }
     }
 }
