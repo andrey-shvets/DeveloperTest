b8ac032263dbdf3909703ddf0ed4f1efc6adb7ed
andrey-shvets <andrey.shvets.sd@yandex.ru>
2022-02-21 04:23:40 +0200

    Add WordReader

---
+++ DeveloperTestFrameworkUnitTests/DeveloperTest/Helpers/TestCharacterReader.cs
@@ -0,0 +1,36 @@
+﻿using DeveloperTestInterfaces;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeveloperTestFramework.DeveloperTest.Helpers
+{
+    internal class TestCharacterReader : ICharacterReader
+    {
+        private string Content { get; }
+        private int _currentPosition;
+
+        public TestCharacterReader(string content)
+        {
+            Content = content;
+        }
+
+        public char GetNextChar()
+        {
+            if (_currentPosition >= Content.Length)
+                throw new EndOfStreamException();
+
+            return Content[_currentPosition++];
+        }
+
+        public async Task<char> GetNextCharAsync(CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+            cancellationToken.ThrowIfCancellationRequested();
+            return GetNextChar();
+        }
+
+        public void Dispose()
+        { }
+    }
+}
---
+++ DeveloperTestFrameworkUnitTests/DeveloperTest/WordReaderTests.cs
@@ -0,0 +1,68 @@
+﻿using DeveloperTest.Reader;
+using DeveloperTestFramework.DeveloperTest.Helpers;
+using NUnit.Framework;
+using System.IO;
+
+namespace DeveloperTestFramework.DeveloperTest
+{
+    [TestFixture]
+    public class WordReaderTests
+    {
+        [TestCase("")]
+        [TestCase(" !!@#$%^&*()_+=-{}[]'\"\\|/?.>,<`~--")]
+        public void GetNextWord_ThrowsEndOfStreamException_ForTextWithNoWords(string content)
+        {
+            var characterReader = new TestCharacterReader(content);
+            var reader = new WordReader(characterReader);
+
+            Assert.Throws<EndOfStreamException>(() => reader.GetNextWord());
+        }
+
+        [TestCase("Word", "word")]
+        [TestCase("Word ", "word")]
+        [TestCase("  WoRd", "word")]
+        [TestCase("word 2ndword", "word")]
+        [TestCase("ninja-monkey!", "ninja-monkey")]
+        [TestCase("ninja--monkey!", "ninja")]
+        [TestCase(" -Word", "word")]
+        [TestCase("Word'.", "word")]
+        [TestCase("Word-!", "word")]
+        public void GetNextWord_ReadsWord_FromReaderWithSingleWord(string content, string expected)
+        {
+            var characterReader = new TestCharacterReader(content);
+            var reader = new WordReader(characterReader);
+
+            var word = reader.GetNextWord();
+
+            Assert.That(word, Is.EqualTo(expected));
+        }
+
+        [TestCase("first second third.", "second")]
+        [TestCase("first--second", "second")]
+        [TestCase("first --second", "second")]
+        [TestCase("first - second", "second")]
+        [TestCase("first,!? (second)", "second")]
+        [TestCase("first 'second'", "second")]
+        public void GetNextWord_ReadsSecondWord_FromTextWithMultipleWords(string content, string expected)
+        {
+            var characterReader = new TestCharacterReader(content);
+            var reader = new WordReader(characterReader);
+
+            _ = reader.GetNextWord();
+            var word = reader.GetNextWord();
+
+            Assert.That(word, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetNextWord_ThrowsEndOfStreamException_AfterLastWordRead()
+        {
+            var content = "Word !";
+            var characterReader = new TestCharacterReader(content);
+            var reader = new WordReader(characterReader);
+
+            _ = reader.GetNextWord();
+            Assert.Throws<EndOfStreamException>(() => reader.GetNextWord());
+        }
+    }
+}
